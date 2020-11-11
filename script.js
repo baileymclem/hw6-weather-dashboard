8d7534487ea92ea6a01f0fd85674b4671b4cf308
Bailey Clemons <baileythevampireslayer@gmail.com>
2020-11-10 17:30:22 -0800

    "uv index and working on forecast"

--- script.js
+++ script.js
@@ -1,47 +1,76 @@
 //
 
 //once DOM load, run jquery
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var apiKey = '1453ed8356c0e4fbc69da4446bd93e17'
+  var apiKey = '1453ed8356c0e4fbc69da4446bd93e17'
 
-    // var title = "space+jam";
-    // var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+  //search city name
 
-    $('.btn').on('click', function(e){
-        e.preventDefault();
+  $('.btn').on('click', function (e) {
+    e.preventDefault();
 
-        var cityName = $('#cityNameText').val().trim(); 
-
-        console.log('cityName', cityName)
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" +apiKey;
-    
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          console.log('res', response);
-          var temp = response.main.temp;
-          var humidity = response.main.humidity;
-          var windSpeed = response.wind.speed;
-
-          var weatherForecaseEL = $("#weatherForecast")
-
-          var tempEl = $('<p>');
-          tempEl.text(`Temp: ` + temp);
-
-          var humidityEl = $('<p>');
-          humidityEl.text('Humidity ' + humidity);
-
-          weatherForecaseEL.append(tempEl);
-          weatherForecaseEL.append(humidityEl);
+    var cityName = $('#cityNameText').val().trim();
 
 
+    console.log('cityName', cityName)
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+    // var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityId +"&appid=" + apiKey;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log('res', response);
+      var name = response.name;
+      var temp = response.main.temp;
+      var humidity = response.main.humidity;
+      var windSpeed = response.wind.speed;
+      var uvIndex = response.wind.speed;
+      var cityId = response.id;
+
+      console.log("cityId", cityId);
 
 
-        });
+      var weatherForecaseEL = $("#weatherForecast")
+
+      var nameEl = $('<h1>');
+      nameEl.text(name);
+
+      var tempEl = $('<p>');
+      tempEl.text(`Temp: ` + temp);
+
+      var humidityEl = $('<p>');
+      humidityEl.text('Humidity: ' + humidity);
+
+      var windSpeedEl = $('<p>');
+      windSpeedEl.text('Wind Speed: ' + windSpeed);
+
+      var uvIndexEl = $('<p>');
+      uvIndexEl.text(`UV Index: ` + uvIndex);
+
+      weatherForecaseEL.append(nameEl);
+      weatherForecaseEL.append(tempEl);
+      weatherForecaseEL.append(humidityEl);
+      weatherForecaseEL.append(windSpeedEl);
+      weatherForecaseEL.append(uvIndexEl);
+
+
+
 
     })
+
+    // $.ajax({
+    //   url: queryURL2,
+    //   method: "GET"
+    // }).then(function (response) {
+
+    // })
+
+  })
+
+
 
 
 })
